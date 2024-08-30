0a80c399c0e627941adf33f47853bbb8228859c8
DaNYwwww <162842307+DaNYwwww@users.noreply.github.com>
2024-08-30 13:46:17 +1000

    Create Практическое задание по теме "Переменные".py

---
+++ Практическое задание по теме "Переменные".py
@@ -0,0 +1,6 @@
+Numbers_of_homework = 12
+Numbers_of_hours = 1.5
+Class = "Python"
+Time_for_the_one_task = Numbers_of_hours / Numbers_of_homework
+print("Курс:",Class,',', "всего задач:", Numbers_of_homework, ',',"затрачено часов:", Numbers_of_hours,',', "среднее время выполнения", Time_for_the_one_task, "часов")
+
