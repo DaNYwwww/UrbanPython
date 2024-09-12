16f38a032896b32668ec71b34bf40a178616deda
DaNYwwww <162842307+DaNYwwww@users.noreply.github.com>
2024-09-12 21:23:52 +1000

    Update module_1_7*.py

--- module_1_7*.py
+++ module_1_7*.py
@@ -1,6 +1,7 @@
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students_list=list(students)
+students_list.sort()
 average_grades={}
 for students in students_list:
     students_grades = grades[students_list.index(students)]
